10705e79123cea4566f3c37fb26fa8090a38aee3
adeDinataCandra <candra.ade21@gmail.com>
2019-07-09 23:22:04 -0700

    versi 01

---
+++ mvc.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.5.5-10.1.10-MariaDB : Database - mvc
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mvc` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mvc`;
+
+/*Table structure for table `mahasiswa` */
+
+DROP TABLE IF EXISTS `mahasiswa`;
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image` varchar(100) NOT NULL,
+  `nama` varchar(60) NOT NULL,
+  `nrp` varchar(60) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `jurusan` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+
+/*Data for the table `mahasiswa` */
+
+insert  into `mahasiswa`(`id`,`image`,`nama`,`nrp`,`email`,`jurusan`) values (35,'','Ade Candra','15321','candra.ade21@gmail.com','Teknik Industri'),(36,'','Andre','2012140305','andre@gmail.com','Teknik Informatika'),(37,'','Yayu','15321','yayu@gmail.com','Teknik Informatika');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
